c39007f5bf6590991915dce4340b62f85064db35
vellt <szantobenjamin97@gmail.com>
2024-10-02 10:09:46 +0200

    mai ora

---
+++ WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            csuszka.ValueChanged += Csuszka_ValueChanged;
+        }
+
+        private void Csuszka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int ertek = (int)csuszka.Value;
+            kep.Source = new BitmapImage(new Uri($"{ertek}.png", UriKind.Relative));
+        }
+    }
+}
