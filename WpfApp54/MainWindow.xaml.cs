16f4aee374c81f59d32249a1ce24b4e735794fa8
vellt <szantobenjamin97@gmail.com>
2024-12-21 10:58:44 +0100

    mai ora

---
+++ WpfApp54/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp54
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseUp += MainWindow_MouseUp;
+        }
+
+        private void MainWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (konyv.Visibility==Visibility.Visible)
+            {
+                konyv.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                konyv.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
