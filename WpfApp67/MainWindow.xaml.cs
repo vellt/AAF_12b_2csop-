4681275d40422ac9ad350665635e7d222e125dc7
vellt <szantobenjamin97@gmail.com>
2025-01-15 19:02:31 +0100

    mai ora

---
+++ WpfApp67/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using System.Windows.Shapes;
+
+namespace WpfApp67
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> szovegek = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += Button_Click;
+            szovegek = File.ReadAllLines("meme_szovegek.csv").ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random r = new Random();
+            int index = r.Next(szovegek.Count);
+            szoveg.Text = szovegek[index];
+
+            int index2 = r.Next(1, 70);
+            kep.Source = new BitmapImage(new Uri($"kepek/{index2}.jpg",UriKind.Relative));
+        }
+    }
+}
