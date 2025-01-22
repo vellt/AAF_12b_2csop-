49400e27cc43396bb2d17a7e33761b7f6990bb6b
vellt <szantobenjamin97@gmail.com>
2025-01-22 10:00:48 +0100

    mai ora

---
+++ WpfApp76/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp76
+{
+    class Varos
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Varos> varosok = new List<Varos>()
+            {
+                new Varos() { Name="Berlin",  Value= "Berlin" },
+                new Varos() { Name="Tokió",  Value= "Tokyo" },
+                new Varos() { Name="Rio",  Value= "Rio" },
+                new Varos() { Name="Nairobi",  Value= "Nairobi" },
+                new Varos() { Name="Lisszabon",  Value= "Lisbon" },
+                new Varos() { Name="Moszkva",  Value= "Moscow" },
+                new Varos() { Name="Denver",  Value= "Denver" },
+                new Varos() { Name="Stockholm",  Value= "Stockholm" },
+                new Varos() { Name="Helsinki",  Value= "Helsinki" },
+                new Varos() { Name="Professzor",  Value= "Professor" },
+            };
+            foreach (var varos in varosok)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = varos.Name;
+                item.Tag = varos.Value;
+                varosokTallozo.Items.Add(item);
+            }
+
+            //varosokTallozo.ItemsSource = varosok;
+            varosokTallozo.SelectionChanged += VarosokTallozo_SelectionChanged;
+        }
+
+        private void VarosokTallozo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string magyar = (varosokTallozo.SelectedItem as ComboBoxItem).Content.ToString();
+            string angol = (varosokTallozo.SelectedItem as ComboBoxItem).Tag.ToString();
+            varosNev.Text = magyar;
+            Image image = new Image();
+            image.Height = 200;
+            image.Source = new BitmapImage(new Uri($"images/{angol}.jpg", UriKind.Relative));
+            kep.Children.Clear();
+            kep.Children.Add(image);
+        }
+    }
+}
