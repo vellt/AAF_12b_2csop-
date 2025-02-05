552c69735209e3e63585453e4b9053b581763e92
vellt <szantobenjamin97@gmail.com>
2025-02-05 13:24:06 +0100

    mai ora

---
+++ ConsoleApp128/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp128
+{
+    class Nobeldijas
+    {
+        public int Ev { get; set; }
+        public string Tipus { get; set; }
+        public string Knev { get; set; }
+        public string Vnev { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Nobeldijas> adatok = ReadDataFromFile();
+            //Mennyi adatsorból áll a fájl?
+            Console.WriteLine($"{adatok.Count()} db");
+
+            //Listázd ki az adatsorokat(minden adattagot).
+            adatok.Select(x => $"{x.Ev} {x.Tipus} {x.Vnev} {x.Knev}")
+                .ToList().ForEach(x => Console.WriteLine(x));
+
+            //Rendezd azon Nóbel - díjasokat keresztnév szerint növekvő sorrendbe, 
+            //melyek 2014 után kaptak orvosi Nóbel-díjat.
+            // A kiíratás Vezetéknév és Keresztnév formájában történjen. 
+            adatok.OrderBy(x => x.Knev)
+                .Where(x => x.Ev > 2014 && x.Tipus == "orvosi")
+                .Select(x => $"{x.Vnev} {x.Knev}")
+                .ToList().ForEach(x => Console.WriteLine(x));
+
+            //Mennyi típus van(duplikációkat eltünteted, majd megszámolod) ?
+            int db =adatok.Select(x => x.Tipus).Distinct().Count();
+            Console.WriteLine(db);
+
+            //Listázza ki évszám szerint növekvően az 1960 és 1970 között 
+            // irodalmi Nobel - díjat nyert írók nevét(vezetéknév + keresztnév).
+            // A vizsgált időszakba a határok is beletartoznak.
+            adatok.Where(x => x.Ev >= 1960 && x.Ev <= 1970 && x.Tipus == "irodalmi")
+                .OrderBy(x => x.Ev).Select(x => $"Név: {x.Vnev} {x.Knev}")
+                .ToList().ForEach(x => Console.WriteLine(x));
+
+            //Melyik típusban mennyi Nóbel-díjas van?
+            adatok.GroupBy(x => x.Tipus).Select(x => $"típus: {x.Key}, {x.Count()}db")
+                .ToList().ForEach(x => Console.WriteLine(x));
+
+            //Melyik évben mennyi Nóbel - díjas volt ?
+            adatok.GroupBy(x => x.Ev).Select(x => $"{x.Key}: {x.Count()}db")
+                .ToList().ForEach(x => Console.WriteLine(x));
+
+            //2015 - ben mennyi Nóbel - díjas volt?
+            int db2=  adatok.Where(x => x.Ev == 2015).Count();
+            Console.WriteLine($"{db2}db");
+
+            // Kérj be egy nevet és listázd azon Nóbel - díjasokat, 
+            // akinek az a vezeték vagy keresztneve, amit a felhasználó 
+            // adott meg konzolból, a találatok számát is írd ki.
+            Console.Write("Név: ");
+            string nev = Console.ReadLine();
+
+            List<string> talalatok = adatok.Where(x => x.Knev == nev || x.Vnev == nev)
+                .Select(x => $"Név: {x.Vnev} {x.Knev}").ToList();
+
+            Console.WriteLine($"találatok száma: {talalatok.Count()}db");
+            talalatok.ForEach(x => Console.WriteLine(x));
+
+
+            Console.ReadKey();
+        }
+
+        static List<Nobeldijas> ReadDataFromFile()
+        {
+            return File.ReadAllLines("nobeldijak.csv")
+                .Skip(1)
+                .Select(x => x.Split(';'))
+                .Select(x => new Nobeldijas()
+                {
+                    Ev=Convert.ToInt32(x[0]),
+                    Tipus=x[1],
+                    Knev=x[2],
+                    Vnev=x[3]
+                }).ToList();
+        }
+    }
+}
--- ConsoleApp128/Properties/AssemblyInfo.cs
+++ ConsoleApp128/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hegycsucsokLINQ")]
+[assembly: AssemblyTitle("ConsoleApp128")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hegycsucsokLINQ")]
+[assembly: AssemblyProduct("ConsoleApp128")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a6fdca51-6fb8-4b6d-b16a-fab0cd78f82f")]
+[assembly: Guid("00a98464-57c4-4bb6-bd8e-aea1cc75544b")]
 
 // Version information for an assembly consists of the following four values:
 //
